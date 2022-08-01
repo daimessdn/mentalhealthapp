1a5f8e3b548f98290f92f9f99bfd8ddf7e148158
daimessdn@gmail.com <daimessdn@gmail.com>
2022-08-01 07:50:26 +0700

    added top user welcome and physical search feature in homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,73 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[800],
-    );
+        backgroundColor: Colors.blue[800],
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              children: [
+                // welcome message user
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hi, Bambang!",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          "23 Jan, 2021",
+                          style: TextStyle(color: Colors.blue[200]),
+                        )
+                      ],
+                    ),
+
+                    // bagian notifikasi
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.blue[600],
+                          borderRadius: BorderRadius.circular(12)),
+                      padding: EdgeInsets.all(16),
+                      child: Icon(
+                        Icons.notifications,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+
+                // bagian box untuk margin
+                SizedBox(height: 20),
+
+                // bagian search
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.blue[600],
+                      borderRadius: BorderRadius.circular(12)),
+                  padding: EdgeInsets.all(12),
+                  child: Row(children: [
+                    Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("Search", style: TextStyle(color: Colors.white)),
+                  ]),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
